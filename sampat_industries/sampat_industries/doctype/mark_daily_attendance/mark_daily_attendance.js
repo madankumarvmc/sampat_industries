7be4e1379f2e770eb79d0af0679a6812bdd2a1b0
Ubuntu <ubuntu@ip-172-31-82-20.ec2.internal>
2023-09-10 08:58:13 +0000

    wip

--- sampat_industries/sampat_industries/doctype/mark_daily_attendance/mark_daily_attendance.js
+++ sampat_industries/sampat_industries/doctype/mark_daily_attendance/mark_daily_attendance.js
@@ -89,34 +89,6 @@ frappe.ui.form.on('Mark Daily Attendance', {
 					
 					updateTotalHrs(e.LOG_IN, e.LOG_OUT )
 
-
-					// // Example in time and out time in 24-hour format (HH:mm)
-					// const inTime = e.LOG_IN;
-					// const outTime = e.LOG_OUT;
-
-					// // Split the time strings into hours and minutes
-					// const inTimeParts = inTime.split(":");
-					// const outTimeParts = outTime.split(":");
-
-					// // Create Date objects for in time and out time
-					// const inDateTime = new Date();
-					// inDateTime.setHours(parseInt(inTimeParts[0], 10));
-					// inDateTime.setMinutes(parseInt(inTimeParts[1], 10));
-
-					// const outDateTime = new Date();
-					// outDateTime.setHours(parseInt(outTimeParts[0], 10));
-					// outDateTime.setMinutes(parseInt(outTimeParts[1], 10));
-
-					// // Calculate the time difference in milliseconds
-					// const timeDifferenceMs = outDateTime - inDateTime;
-
-					// // Convert the time difference to hours and minutes
-					// const hours = Math.floor(timeDifferenceMs / (1000 * 60 * 60));
-					// const minutes = Math.round((timeDifferenceMs % (1000 * 60 * 60)) / (1000 * 60));
-
-					// console.log(`Total working hours: ${hours} hours ${minutes} minutes`);
-
-
 					if (e.attendance == "Present") {
 						// entry.absent = "0"
 					} else if (e.attendance == "Absent") {
@@ -129,6 +101,7 @@ frappe.ui.form.on('Mark Daily Attendance', {
 					}
 				})
 				refresh_field("daily_attendance_child");
+				// frm.refresh_field('total_hrs');
 				})
 
 
@@ -177,106 +150,87 @@ frappe.ui.form.on('Mark Daily Attendance', {
 
 		validate: function(frm) {
 			let data = frm.doc.daily_attendance_child;
-			$.each(data, function(_i, e) {
-				
+		
+			function updateEmployeeCheckin(employee, time, loginType, checkinid, date) {
+				frappe.call({
+					method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.update_to_employee_checkin",
+					args: { employee, time, login_type: loginType, checkinid, date }
+				}).done(() => {
+					refresh_field("daily_attendance_child");
+				});
+			}
+		
+			function addEmployeeCheckin(employee, time, loginType, checkinid, date) {
+				frappe.call({
+					method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_employee_checkin",
+					args: { employee, time, login_type: loginType, checkinid, date }
+				}).done(() => {
+					refresh_field("daily_attendance_child");
+				});
+			}
+		
+			data.forEach(e => {
 				if (e.in_time) {
+					const time = Date.parse(frm.doc.date + "T" + e.in_time);
+		
 					if (e.login_checkinid) {
-						// console.log("if running !!!!!!!")
-
-						var dateObj = new Date(frm.doc.date);
-
-
-						let time = Date.parse(frm.doc.date + "T" + e.in_time);
-						let time1 = new Date(frm.doc.date + " " + e.in_time);
-						// console.log(time1)
-						    
-						frappe.call({
-							method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.update_to_employee_checkin",
-							args: {employee: e.employee, time: e.in_time, login_type: "IN", checkinid: e.login_checkinid, date: frm.doc.date }
-						}).done((r) => {
-								
-							})
-							refresh_field("daily_attendance_child");
+						updateEmployeeCheckin(e.employee, e.in_time, "IN", e.login_checkinid, frm.doc.date);
 					} else {
-						// console.log("else running !!!!!!!")
-						let time1 = new Date(frm.doc.date + " " + e.in_time);
-
-						let time = Date.parse(frm.doc.date + "T" + e.in_time);
-						// console.log(time1);
-						frappe.call({
-							method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_employee_checkin",
-							args: {employee: e.employee, time: e.in_time, login_type: "IN", checkinid: e.login_checkinid, date: frm.doc.date }
-						}).done((r) => {
-								
-							})
-							refresh_field("daily_attendance_child");
+						addEmployeeCheckin(e.employee, e.in_time, "IN", e.login_checkinid, frm.doc.date);
 					}
 				}
 		
 				if (e.out_time) {
+					const time = Date.parse(frm.doc.date + "T" + e.out_time);
+		
 					if (e.logout_checkinid) {
-						frappe.call({
-							method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.update_to_employee_checkin",
-							args: {employee: e.employee, time: e.out_time, login_type: "OUT", checkinid: e.logout_checkinid, date: frm.doc.date }
-						}).done((r) => {
-								
-							})
-							refresh_field("daily_attendance_child");
-					} 
-					else {
-						// console.log("else running !!!!!!!")
-						let time = Date.parse(frm.doc.date + "T" + e.in_time);
-						// console.log(time);
-						frappe.call({
-							method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_employee_checkin",
-							args: {employee: e.employee, time: e.out_time, login_type: "OUT", checkinid: e.logout_checkinid, date: frm.doc.date }
-						}).done((r) => {
-								
-							})
-							refresh_field("daily_attendance_child");
+						updateEmployeeCheckin(e.employee, e.out_time, "OUT", e.logout_checkinid, frm.doc.date);
+					} else {
+						addEmployeeCheckin(e.employee, e.out_time, "OUT", e.logout_checkinid, frm.doc.date);
 					}
 				}
+		
 
-
-				// console.log(e.attendance_id)
-
-				// console.log(e.absent)
-				if (e.in_time !== null && e.attendance_id == null ) {
-					// console.log("Mark Present")
-					// Mark Present in Attendance Doctype
+				// Define a function to mark attendance
+				function markAttendance(employee, date, company, status, leaveType = null) {
 					frappe.call({
 						method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_attendance",
-						args: {employee: e.employee, date: frm.doc.date, company: frm.doc.company, status: "Present", leave_type: "Casual Leave" }
+						args: { employee, date, company, status, leave_type: leaveType },
 					}).done((r) => {
-							
-						})
-				} else if(e.in_time == null && e.absent == "1" && e.approved == "0" && e.attendance_id == null) {
-					// Mark Absent
-					// console.log("Mark Absent")
-					frappe.call({
-						method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_attendance",
-						args: {employee: e.employee, date: frm.doc.date, company: frm.doc.company, status: "Absent", leave_type: "Casual Leave" }
-					}).done((r) => {
-							
-						})
-
-				} else if (e.in_time == null && e.absent == "1" && e.approved == "1" && e.attendance_id == null) {
-					// Mark On Leave
-					// console.log("Mark On Leave")
-					frappe.call({
-						method: "sampat_industries.sampat_industries.doctype.mark_daily_attendance.mark_daily_attendance.add_to_attendance",
-						args: {employee: e.employee, date: frm.doc.date, company: frm.doc.company, status: "On Leave", leave_type: "Casual Leave" }
-					}).done((r) => {
-							
-						})
-				} else {
-					// console.log("Do Nothing")
+						// Handle the response if needed
+					});
 				}
 
+				// Check conditions and mark attendance
+				if (e.attendance_id == null && e.in_time !== null) {
+						// Mark Present	
+					markAttendance(e.employee, frm.doc.date, frm.doc.company, "Present");
+
+					} else if (e.absent === "1" && e.approved === "0") {
+						// Mark Absent
+						markAttendance(e.employee, frm.doc.date, frm.doc.company, "Absent");
+					} else if (e.absent === "1" && e.approved === "1") {
+						// Mark On Leave
+						markAttendance(e.employee, frm.doc.date, frm.doc.company, "On Leave", "Casual Leave");
+					} else {
+						// Do Nothing
+					}
 				
 			});
 
 		},
+
+
+		onload: (frm) => {
+			let dailyAttendanceChild = frm.doc.daily_attendance_child;
+			dailyAttendanceChild.forEach((row) => {
+				console.log({row})
+				row.total_hrs = updateTotalHrs(row.in_time, row.out_time)
+				console.log(row.total_hrs)
+			})
+			// frm.refresh_field('total_hrs');
+		},
+
 
 		after_save: function (frm) {
 			frappe.call({
@@ -345,62 +299,12 @@ function updateTotalHrs(in_time, out_time) {
         const duration = moment.duration(timeDiff);
         const totalHrs = Math.floor(duration.asHours()) + moment.utc(timeDiff).format(":mm");
 
-        frappe.model.set_value('total_hrs', totalHrs);
-		console.log("Total Hrs Calculated")
+        // frappe.model.set_value('total_hrs', totalHrs);
+		// console.log("Total Hrs Calculated")
+
+		return totalHrs;
     }
 }
-
-
-
-// frappe.ui.form.on("Daily Attendance Child", "in_time", function(frm, cdt, cdn){
-//     cur_frm.doc.daily_attendance_child.forEach(function(child){
-//         var sel = 'div[data-fieldname="daily_attendance_child"] > div.grid-row[data-idx="' + child.idx + '"]';
-// 		console.log(sel)
-//         if (child.in_time !== '00:00:00'){
-//             $(sel).css('background-color', 'lightgreen');
-//         } else {
-//             $(sel).css('background-color', 'transparent');
-//         }
-//     });
-// });
-
-
-
-
-
-// frappe.ui.form.on('Daily Attendance Child', {
-// 	refresh: function(frm) {
-// 		console.log("hello!!!")
-// 		// console.log(frm.doc)
-// 	//   frm.fields_dict['scrap_items_section'].wrapper.css('background-color', '#FFFFE0');
-// 	//   frm.fields_dict['workstation_mapping'].wrapper.css('background-color', '#90EE90');
-// 	}
-//   });
-
-// frappe.ui.form.on('Daily Attendance Child', {
-//     refresh: function(frm, cdt, cdn) {
-// 		console.log("hello!!!!")
-
-
-//         // Get the child table field
-//         var child_table = locals[cdt][cdn];
-
-//         // Iterate through the rows in the child table
-//         child_table.daily_attendance_child.forEach(function(row) {
-//             // Check if the in_time is not "00:00:00"
-//             if (row.in_time !== "00:00:00") {
-//                 // Add a CSS class to mark the cell as green
-//                 frm.fields_dict.daily_attendance_child.grid.get_field('in_time').get_query_status(row).style = 'background-color: #00ff00; color: #fff;';
-//             }
-
-//             // Check if the out_time is not "00:00:00"
-//             if (row.out_time !== "00:00:00") {
-//                 // Add a CSS class to mark the cell as green
-//                 frm.fields_dict.daily_attendance_child.grid.get_field('out_time').get_query_status(row).style = 'background-color: #00ff00; color: #fff;';
-//             }
-//         });
-//     }
-// });
 
 
 
